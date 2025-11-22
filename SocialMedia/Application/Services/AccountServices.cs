b46ee5bcf0f831285811be708ce47b514d7e96a9
Abdelrhmancs <ght7234@gmail.com>
2025-11-22 20:21:11 +0200

    add Login services

--- SocialMedia/Application/Services/AccountServices.cs
+++ SocialMedia/Application/Services/AccountServices.cs
@@ -7,22 +7,29 @@ using API.Domain.Entites;
 using Application.DTOs.AccountDTOs;
 using Application.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using Domain.Validation;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Configuration;
 namespace Application.Services
 {
     public class AccountServices : IAccountServices
     {
         private readonly UserManager<User> _userManager;
-        public AccountServices(UserManager<User> userManager)
+        private readonly IConfiguration _config;
+        public AccountServices(UserManager<User> userManager  , IConfiguration configuration)
         {
             _userManager = userManager;
+            _config = configuration;
         }
 
-        public async Task<string> RegisterAsync(RegisterDto dto)
+        public async Task<Result> RegisterAsync(RegisterDto dto)
         {
             var check_exist = await _userManager.FindByEmailAsync(dto.Email);
             if (check_exist != null)
             {
-                return "already exist user ";
+                return Result.Failure(["already exist user "]);
             }
             
             var new_user = new User
@@ -32,12 +39,47 @@ namespace Application.Services
                 Email = dto.Email
             };
             var res = await _userManager.CreateAsync(new_user, dto.Password);
-            if (res.Succeeded)
+            if (!res.Succeeded)
             {
-                return "User registered successfully";
+                return Result.Failure(["error in register"]);
             }
-            return "error in register";
+            
+            return Result.success("User registered successfully");
+        }
 
+        public async Task<ResultT<LoginResponsesDto>> LoginAsync(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if(user == null)
+            {
+                return ResultT<LoginResponsesDto>.Failure(["Email is Wrong or not found"]);
+            }
+
+            var check_password = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+            if (!check_password)
+            {
+                return ResultT<LoginResponsesDto>.Failure(["password is Wrong"]);
+            }
+            var claims = await _userManager.GetClaimsAsync(user);
+      
+
+            SecurityKey securityKey = new SymmetricSecurityKey
+                              (Encoding.UTF8.GetBytes(_config["JWT:secret"]));
+            SigningCredentials signcred = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _config["JWT:ValidIssuer"],
+                audience: _config["JWT:ValidAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(24),
+                signingCredentials: signcred
+            );
+            var responsedData = new LoginResponsesDto
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Email = user.Email,
+                Name = user.Name
+            };
+            return ResultT<LoginResponsesDto>.success(responsedData, "login successfully");
         }
     }
 }
