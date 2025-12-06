eebd909d5c1716ee6126b0d97bc020268575fd1a
Abdelrhmancs <ght7234@gmail.com>
2025-12-06 03:55:20 +0200

    create context (to get  user Id dynamic)

---
+++ SocialMedia/SocialMedia/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using System.Security.Claims;
+
+namespace API
+{
+    public class UserContext : IUserContext
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public UserContext(IHttpContextAccessor httpContext)
+
+        {
+            _httpContext = httpContext;
+        }
+        public string GetUserId()
+        {
+            var user = _httpContext.HttpContext?.User;
+            return user?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+         
+        }
+    }
+}
