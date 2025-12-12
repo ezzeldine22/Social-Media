52c97205b3d32b754b608ae47c077b1ad75f8e4c
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-12 20:14:30 +0200

    makeSearchPostsDTO

---
+++ SocialMedia/Application/DTOs/PostDTOs/SearchPostsDTO.cs
@@ -0,0 +1,22 @@
+﻿using API.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.PostDTOs
+{
+    public class SearchPostsDTO
+    {
+        public string userName { get; set; }
+        public string userPic { get; set; }
+        public string? Caption { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? VideoUrl { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public virtual ICollection<Like> Likes { get; set; } = new List<Like>();
+        public virtual ICollection<Share> Shares { get; set; } = new List<Share>();
+    }
+}
