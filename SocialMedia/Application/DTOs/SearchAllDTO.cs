c8f3b257c7e3ed895317a86929aa02ef11da33a9
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-12 20:14:41 +0200

    makeSearchAllDTO

---
+++ SocialMedia/Application/DTOs/SearchAllDTO.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.PostDTOs;
+using Application.DTOs.UserDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class SearchAllDTO
+    {
+       
+        public IList<SearchPostsDTO> Posts { get; set; }
+        public IList<SearchUsersResultDTO> Users { get; set; }
+    }
+}
