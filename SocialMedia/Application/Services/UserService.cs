e0ec934ef2f3eb5dc4e01045d36e4d69ce7f3f8f
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-12 20:17:18 +0200

    implementSearchAll,SearchUsers,UpdateUserprofile

--- SocialMedia/Application/Services/UserService.cs
+++ SocialMedia/Application/Services/UserService.cs
@@ -1,7 +1,10 @@
 ﻿using API.Domain.Entites;
+using Application.DTOs;
+using Application.DTOs.PostDTOs;
 using Application.DTOs.UserDTOs;
 using Application.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +16,16 @@ namespace Application.Services
     public class UserService : IUserService
     {
         private readonly UserManager<User> _userManager;
+        private readonly IUserContext _userContext;
+        private readonly IRepository<Post> _repoPost;
 
-        public UserService(UserManager<User> userManager)
+        public UserService(UserManager<User> userManager,
+            IUserContext userContext,
+            IRepository<Post> repoPost)
         {
             _userManager = userManager;
+            _userContext = userContext;
+            _repoPost = repoPost;
         }
         public async Task<GetUserProfileDTO> GetUserProfile(string id)
         {
@@ -35,6 +44,95 @@ namespace Application.Services
                 Pic = user.Pic
             };
             return userProfile;
+        }
+
+        public async Task<SearchAllDTO> searchAll(string query, int pageNumber = 1, int pageSize = 10)
+        {
+            var usersQuery = _userManager.Users.AsQueryable().AsNoTracking();
+            var postsQuery = _repoPost.ReadAll();
+            if (!string.IsNullOrEmpty(query))
+            {
+                usersQuery = usersQuery.Where(u =>
+                    EF.Functions.Like(u.Name, $"%{query}%") ||
+                    EF.Functions.Like(u.Email, $"%{query}%") ||
+                    EF.Functions.Like(u.UserName, $"%{query}%")
+                    );
+                postsQuery = postsQuery.Where(p =>
+                    EF.Functions.Like(p.Caption, $"%{query}%"));
+            }
+
+            var users = await usersQuery
+                .OrderBy(u => u.Name)
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .Select(u => new SearchUsersResultDTO
+                {
+                    Name = u.Name,
+                    Pic = u.Pic
+                })
+                .ToListAsync();
+
+            var posts = await postsQuery
+                .OrderByDescending(p => p.CreatedAt)
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .Select(p => new SearchPostsDTO
+                {
+                    Caption = p.Caption,
+                    ImageUrl = p.ImageUrl,
+                    VideoUrl = p.VideoUrl,
+                    CreatedAt = p.CreatedAt,
+                    userName = p.User.Name,
+                    userPic = p.User.Pic
+                })
+                .ToListAsync();
+
+            return new SearchAllDTO
+            {
+                Users = users,
+                Posts = posts
+            };
+        }
+
+        public async Task<IList<SearchUsersResultDTO>> SearchUsers(string query, int pageNumber, int pageSize)
+        {
+            var res = _userManager.Users.AsNoTracking().AsQueryable();
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+             res = res.Where(u=>
+                    EF.Functions.Like(u.Name , $"%{query}%")||
+                    EF.Functions.Like(u.Email , $"%{query}%") ||
+                    EF.Functions.Like(u.UserName , $"%{query}%")    
+                    );
+            }
+            var users =  res.OrderBy(u => u.Name)
+                .ThenBy(u => u.Email)
+                .ThenBy(u => u.UserName)
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .Select(u => new SearchUsersResultDTO
+                {
+                     Name = u.Name,
+                     Pic = u.Pic
+                 }).ToListAsync();
+
+            return await users;
+        }
+
+        public async Task UpdateUserProfile(UpdateUserProfileDTO updateUserProfileDTO)
+        {
+           string userId = _userContext.GetUserId();
+           var user = await _userManager.FindByIdAsync(userId);
+           if(user == null)
+           {
+              // handling logic
+           }
+           user.Name = updateUserProfileDTO.Name;
+           user.Email = updateUserProfileDTO.Email;
+           user.PhoneNumber = updateUserProfileDTO.Phone;
+           user.Bio = updateUserProfileDTO.Bio;
+           user.Pic = updateUserProfileDTO.Pic;
+           await _userManager.UpdateAsync(user);
         }
     }
 }
