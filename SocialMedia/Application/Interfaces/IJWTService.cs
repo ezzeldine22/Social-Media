13edd7236cecf9dde575a9026af6c2240296c443
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:20:17 +0200

    create JWT service

---
+++ SocialMedia/Application/Interfaces/IJWTService.cs
@@ -0,0 +1,16 @@
+﻿using API.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IJWTService
+    {
+        public Task<string> generateTokenAsync(User user, IEnumerable<Claim> claims);
+        public Task<IEnumerable<Claim>> generateClaimsAsync(User user);
+    }
+}
