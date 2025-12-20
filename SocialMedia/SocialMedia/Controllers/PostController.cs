05f27b7f471d3ac230eb72f6f3f260bb21516420
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:27:58 +0200

    Using Use Cases instead of Services In Post Controller

--- SocialMedia/SocialMedia/Controllers/PostController.cs
+++ SocialMedia/SocialMedia/Controllers/PostController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.DTOs.PostDTOs;
 using Application.Interfaces;
+using Application.UseCases.Post;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq.Expressions;
@@ -10,85 +11,80 @@ namespace API.Controllers
     [ApiController]
     public class PostController : ControllerBase
     {
-        private readonly IPost _post;
-        public PostController(IPost post)
+      
+        private readonly CreatePostUseCase _createPostUseCase;
+        private readonly DeletePostUseCase _deletePostUseCase;
+        private readonly GetPostByIDUseCase _getPostByIDUseCase;
+        private readonly GetUserPostsUseCase _getUserPostsUseCase;
+        private readonly SearchPostsUseCase _searchPostsUseCase;
+        private readonly UpdatePostUseCase _updatePostUseCase;
+
+        public PostController(
+            CreatePostUseCase createPostUseCase , 
+            DeletePostUseCase deletePostUseCase , 
+            GetPostByIDUseCase getPostByIDUseCase , 
+            GetUserPostsUseCase getUserPostsUseCase ,
+            SearchPostsUseCase searchPostsUseCase ,
+            UpdatePostUseCase updatePostUseCase)
         {
-            _post = post;
+            _createPostUseCase = createPostUseCase;
+            _deletePostUseCase = deletePostUseCase;
+            _getPostByIDUseCase = getPostByIDUseCase;
+            _getUserPostsUseCase = getUserPostsUseCase;
+            _searchPostsUseCase = searchPostsUseCase;
+            _updatePostUseCase = updatePostUseCase;
         }
 
         [HttpPost("CreatePost")]
         public async Task<IActionResult> CreatePost(PostDto dto)
         {
-            try
-            {
-                var res = await _post.CreatePostAsync(dto);
-                return Ok(res);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            var res = await _createPostUseCase.CreatePostAsync(dto);
+            if (res.IsSuccess)
+                return Ok(res.Message);
+            return BadRequest(res.Errors);
         }
         [HttpGet("Get-all-posts-of-user")]
         public async Task<IActionResult> GetUserPost()
         {
-            try
-            {
-                var res = await _post.GetPostAsync();
-                return Ok(res);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            var res = await _getUserPostsUseCase.GetUserPostsAsync();
+            if (res.IsSuccess)
+                return Ok(res.Data);
+            return BadRequest(res.Errors);
         }
 
         [HttpGet("Get-post-by-Id")]
         public async Task<IActionResult> GetPostById(long postId)
         {
-            try
-            {
-                var res = await _post.GetPostById(postId);
-                return Ok(res);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            var res = await _getPostByIDUseCase.GetPostByIdAsync(postId);
+            if (res.IsSuccess)
+                return Ok(res.Data);
+            return BadRequest(res.Errors);
         }
         [HttpPut("Update-Post")]
         public async Task<IActionResult> UpdatePost(long postId, PostDto dto)
         {
-            try
-            {
-                var res = await _post.UpdatePostAsync(postId, dto);
-                return Ok(res);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            var res = await _updatePostUseCase.UpdatePostAsync(postId, dto);
+            if (res.IsSuccess)
+                return Ok(res.Message);
+            return BadRequest(res.Errors);
         }
 
         [HttpDelete("Delete-Post")]
         public async Task<IActionResult> DeletePost(long postId)
         {
-            try
-            {
-                var res = _post.DeletePostAsync(postId);
-                return Ok(res.Result);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            var res = await _deletePostUseCase.DeletePostAsync(postId);
+            if (res.IsSuccess)
+                return Ok(res.Message);
+            return BadRequest(res.Errors);
         }
 
         [HttpPost("SearchPosts")]
         public async Task<IActionResult> searchPosts(string query, int pageNumber = 1, int pageSize=10)
         {
-            var result = await _post.searchPostsAsync(query, pageNumber, pageSize);  
-            return Ok(result);
+            var result = await _searchPostsUseCase.SearchPostsAsync(query, pageNumber, pageSize); 
+            if (result.IsSuccess)
+            return Ok(result.Data);
+            return BadRequest(result.Errors);
         }
        
     }
