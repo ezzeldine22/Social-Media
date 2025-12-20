3352a00f85e2b13b586a6cb23883089001e0c30a
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:24:01 +0200

    Create Update Post Use Case

---
+++ SocialMedia/Application/UseCases/Post/UpdatePostUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs.PostDTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Post
+{
+    public class UpdatePostUseCase
+    {
+        private readonly IPost _post;
+
+        public UpdatePostUseCase(IPost post)
+        {
+            _post = post;
+        }
+        public async Task<Result> UpdatePostAsync(long postId, PostDto dto)
+        {
+            var result = await _post.UpdatePostAsync(postId, dto);
+            return result;
+        }
+    }
+}
