157fd1f0c478f50a30b41d24a4355bdded4e081a
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:20:54 +0200

    create repository for searching

---
+++ SocialMedia/Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using API.Domain.Entites;
+using Application.DTOs;
+using Application.DTOs.UserDTOs;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<SearchUsersResultDTO>> searchUsers(string query, int pageNumber, int pageSize);
+        Task<SearchAllDTO> searchAllAsync(string query, int pageNumber, int pageSize);
+       
+    }
+}
