5b554dcac21de47394d7000aea6517fffa9dc8fd
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:28:12 +0200

    Using Use Cases instead of Services In User Controller

--- SocialMedia/SocialMedia/Controllers/UserController.cs
+++ SocialMedia/SocialMedia/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.DTOs;
 using Application.DTOs.UserDTOs;
 using Application.Interfaces;
+using Application.UseCases.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -9,38 +10,67 @@ namespace API.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly IUserService _userService;
+        
+        private readonly SearchUsersUseCase _searchUsersUseCase;
+        private readonly SearchAllUseCase _searchAllUseCase;
+        private readonly UpdateUserProfileUseCase _updateProfileUseCase;
+        private readonly GetUserProfileUseCase _userProfileUseCase;
 
-        public UserController(IUserService userService)
+        public UserController(
+              SearchUsersUseCase searchUsersUserCase
+            , SearchAllUseCase searchAllUseCase
+            , UpdateUserProfileUseCase updateProfileUseCase
+            , GetUserProfileUseCase userProfileUseCase
+            )
         {
-            _userService = userService;
+            _searchUsersUseCase = searchUsersUserCase;
+            _searchAllUseCase = searchAllUseCase;
+            _updateProfileUseCase = updateProfileUseCase;
+            _userProfileUseCase = userProfileUseCase;
         }
 
-        [HttpGet("GetUserProfile/{id}")]
-        public async Task<GetUserProfileDTO> GetUserProfile(string id)
+        [HttpGet("GetUserProfile")]
+        public async Task<IActionResult> GetUserProfile()
         {
-            return await _userService.GetUserProfile(id);
+            var result = await _userProfileUseCase.GetUserProfile();
+            if (!result.IsSuccess)
+            {
+                return BadRequest(new { error = result.Errors });
+            }
+            return Ok(result.Data);
         }
 
         [HttpGet("SearchUsers")]
         public async Task<IActionResult> SearchUsers(string query, int pageNumber = 1, int pageSize = 10)
         {
-            var res = await _userService.SearchUsers(query, pageNumber, pageSize);
-            return Ok(res);
+            var res = await _searchUsersUseCase.SearchUsers(query, pageNumber, pageSize);
+            if (!res.IsSuccess)
+            {
+                return BadRequest(new { Errors = res.Errors });
+            }
+            return Ok(res.Data);
         }
 
         [HttpGet("SearchAll")]
         public async Task<IActionResult> SearchAll(string query, int pageNumber = 1, int pageSize = 10)
         {
-            var res = await _userService.searchAll(query, pageNumber, pageSize);
-            return Ok(res);
+            var res = await _searchAllUseCase.searchAllAsync(query, pageNumber, pageSize);
+            if (!res.IsSuccess)
+            {
+                return BadRequest(new { Errors = res.Errors });
+            }
+            return Ok(res.Data);
         }
 
         [HttpPost("UpdateUserProfile")]
         public async Task<IActionResult> UpdateUserProfile([FromBody] UpdateUserProfileDTO updateUserProfileDTO)
         {
-            await _userService.UpdateUserProfile(updateUserProfileDTO);
-            return Ok();
+           var result =  await _updateProfileUseCase.UpdateUserProfile(updateUserProfileDTO);
+
+            if (!result.IsSuccess)
+                return BadRequest(new { errors = result.Errors });
+            return Ok(result.Message);
+           
         }
     }
 }
