ac30954f39f0fb1a34ac2da2a11fbf44a156a5d1
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:21:44 +0200

    create following use case

---
+++ SocialMedia/Application/UseCases/Following/FollowingUseCase.cs
@@ -0,0 +1,28 @@
+﻿using API.Domain.Entites;
+using Application.DTOs.FollowingDTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Following
+{
+    public class FollowingUseCase
+    {
+        private readonly IFollowing _followRepo;
+
+        public FollowingUseCase(IFollowing following)
+        {
+            _followRepo = following;
+        }
+        public async Task<Result> FollowAsync(string followedId)
+        {
+            var result = await _followRepo.FollowAsync(followedId);
+            return result;
+        }
+
+    }
+}
