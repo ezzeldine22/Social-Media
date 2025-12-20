8a764d16765d928f12ae307cbcdd0e8263c2360d
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:22:25 +0200

    create unfollow use case

---
+++ SocialMedia/Application/UseCases/Following/UnFollowUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs.FollowingDTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Following
+{
+    public class UnFollowUseCase
+    {
+        private readonly IFollowing _following;
+        
+        public UnFollowUseCase(IFollowing following)
+        {
+            _following = following;
+        }
+        public async Task<Result> UnFollowAsync(string unfollowedId)
+        {
+            var result = await _following.UnFollowAsync(unfollowedId);
+            return result;
+        }
+    }
+}
