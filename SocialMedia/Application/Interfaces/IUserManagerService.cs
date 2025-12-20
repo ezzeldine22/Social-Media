fc7683dd0ad0c81615e8603d683fd765cfeeaea4
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:20:33 +0200

    create user manager service

---
+++ SocialMedia/Application/Interfaces/IUserManagerService.cs
@@ -0,0 +1,21 @@
+﻿using API.Domain.Entites;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IUserManagerService
+    {
+        public Task<User> findByEmailAsync(string email);
+        public Task<User> findByIdAsync(string userId);
+        public Task<IdentityResult> createAsync(User user , string password);
+        public Task<bool> checkPasswordAsync(User user , string password);
+        public Task<IdentityResult> addClaimsAsync(User user , IEnumerable<Claim> claims);
+        public Task<IdentityResult> updateAsync(User user);
+    }
+}
