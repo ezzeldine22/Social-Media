011fb465ef3161bb4abeac85f3a5b7a5f1f15241
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:24:31 +0200

    Create Search for Posts and Users Use Case

---
+++ SocialMedia/Application/UseCases/User/SearchAllUseCase.cs
@@ -0,0 +1,35 @@
+﻿using Application.DTOs;
+using Application.DTOs.PostDTOs;
+using Application.DTOs.UserDTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.User
+{
+    public class SearchAllUseCase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public SearchAllUseCase(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<ResultT<SearchAllDTO>> searchAllAsync(string query, int pageNumber = 1, int pageSize = 10)
+        {
+          var result = await _userRepository.searchAllAsync(query, pageNumber, pageSize);
+
+            if(result.Posts.Any() || result.Users.Any())
+                return ResultT<SearchAllDTO>.success(result);
+            var errors = new List<string>();
+            errors.Add("there is no results to show");
+            return ResultT<SearchAllDTO>.Failure(errors);
+        }
+    }
+}
