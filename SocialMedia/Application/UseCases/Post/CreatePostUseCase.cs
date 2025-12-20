d2b8e89169bfd21e5c700be81967392b713ffb02
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:22:41 +0200

    creat creating post use case

---
+++ SocialMedia/Application/UseCases/Post/CreatePostUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs.PostDTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Post
+{
+    public class CreatePostUseCase
+    {
+        private readonly IPost _post;
+
+        public CreatePostUseCase(IPost post)
+        {
+            _post = post;
+        }
+        public async Task<Result> CreatePostAsync(PostDto postDto)
+        {
+            var result = await _post.CreatePostAsync(postDto);
+            return result;
+        }
+    }
+}
