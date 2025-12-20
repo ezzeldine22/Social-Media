9738e2b9460686dafeafdc381fee343514a9acbe
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:23:47 +0200

    Create Search Posts Use Case

---
+++ SocialMedia/Application/UseCases/Post/SearchPostsUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Application.DTOs.PostDTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Post
+{
+    public class SearchPostsUseCase
+    {
+        private readonly IPost _post;
+
+        public SearchPostsUseCase(IPost post)
+        {
+            _post = post;
+        }
+        public async Task<ResultT<IList<SearchPostsDTO>>> SearchPostsAsync(string query, int pageNumber = 1, int pageSize = 10)
+        {
+            var result = await _post.searchPostsAsync(query, pageNumber, pageSize);
+            if (!result.Any())
+            {
+                var errors = new List<string>();
+                errors.Add("There is no posts to show");
+                return ResultT<IList<SearchPostsDTO>>.Failure(errors);
+            }
+            return ResultT<IList<SearchPostsDTO>>.success(result);
+        }
+    }
+}
