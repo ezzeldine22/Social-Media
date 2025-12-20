9de0d0ae1d2dd494812c6e389532b276a0121f7f
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:23:02 +0200

    Create Delete Post Use Case

---
+++ SocialMedia/Application/UseCases/Post/DeletePostUseCase.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Post
+{
+    public class DeletePostUseCase
+    {
+        private readonly IPost _post;
+
+        public DeletePostUseCase(IPost post)
+        {
+            _post = post;
+        }
+        public async Task<Result> DeletePostAsync(long postId)
+        {
+            var result = await _post.DeletePostAsync(postId);
+            return result;
+        }
+    }
+}
