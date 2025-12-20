c5ef5540eb046bfe0d1402e80961dbae980d3ae1
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-20 08:23:14 +0200

    Create Get Post By Id Use Case

---
+++ SocialMedia/Application/UseCases/Post/GetPostByIDUseCase.cs
@@ -0,0 +1,34 @@
+﻿using Application.DTOs.PostDTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Post
+{
+    public class GetPostByIDUseCase
+    {
+        private readonly IPost _post;
+
+        public GetPostByIDUseCase(IPost post)
+        {
+            _post = post;
+        }
+
+        public async Task<ResultT<PostAllDetailsDtos>> GetPostByIdAsync(long postId)
+        {
+            var result = await _post.GetPostById(postId);
+
+            if (result == null)
+            {
+                var errors = new List<string>();
+                errors.Add("There is no post to show");
+                return ResultT<PostAllDetailsDtos>.Failure(errors);
+            }
+            return ResultT<PostAllDetailsDtos>.success(result);
+        }
+    }
+}
