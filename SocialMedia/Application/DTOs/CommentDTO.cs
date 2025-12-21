ac66c635f38efa26d6288b8f019528c931bdce3b
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-21 13:00:07 +0200

    Create Add Comment DTO

---
+++ SocialMedia/Application/DTOs/CommentDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class CommentDTO
+    {
+        public long postID { get; set; }
+        public string comment { get; set; }
+    }
+}
