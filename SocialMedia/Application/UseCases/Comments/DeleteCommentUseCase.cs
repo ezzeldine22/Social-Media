546d266c2bbf3287be7750d16a03f752e6ae445a
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-21 13:02:10 +0200

    Create Delete Comment Use Case

---
+++ SocialMedia/Application/UseCases/Comments/DeleteCommentUseCase.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Comments
+{
+    public class DeleteCommentUseCase
+    {
+        private readonly IPost _post;
+        public DeleteCommentUseCase(IPost post)
+        {
+            _post = post;
+        }
+        public async Task<Result> deleteCommentAsync(long commentId)
+        {
+            var result = await _post.deleteCommentAsync(commentId);
+            return result;
+        }
+    }
+}
