9e41ef42b6ba11bedcb357a03710957b1866fac8
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-21 13:02:20 +0200

    Create Get Post Comments Use Case

---
+++ SocialMedia/Application/UseCases/Comments/GetPostCommentsUseCase.cs
@@ -0,0 +1,25 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Comments
+{
+    public class GetPostCommentsUseCase
+    {
+        private readonly IPost _postRepo;
+        public GetPostCommentsUseCase(IPost postRepo)
+        {
+            _postRepo = postRepo;
+        }
+        public async Task<ResultT<IEnumerable<GetCommentDTO>>> GetPostCommentsAsync(long postId)
+        {
+            var result = await _postRepo.GetPostCommentsAsync(postId);
+            return result;
+        }
+    }
+}
