86a1a77892f3603244caed491f491562c8c517f8
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-21 13:01:52 +0200

    Create Add Comment Use Case

---
+++ SocialMedia/Application/UseCases/Comments/AddCommentUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Comments
+{
+    public class AddCommentUseCase
+    {
+        private readonly IPost _postRepo;
+
+        public AddCommentUseCase(IPost postRepo)
+        {
+            _postRepo = postRepo;
+        }
+        public async Task<Result> addCommentAsync(CommentDTO commentDTO)
+        {
+            var result = await _postRepo.AddCommentToPost(commentDTO);
+            return result;
+        }
+    }
+}
