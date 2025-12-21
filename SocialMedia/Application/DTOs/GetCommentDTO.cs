026c5ddbe0b0751b9b5eeb66e307dac06b15a14a
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-21 13:00:36 +0200

    Create Get Comment DTO

---
+++ SocialMedia/Application/DTOs/GetCommentDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class GetCommentDTO
+    {
+        public string Text { get; set; }
+        public long CommentID { get; set; } 
+        public string userPic { get; set; }
+        public string userId { get; set; }
+        public string userName { get; set; }
+        public DateTime? CommmentDateTime { get; set; } 
+    }
+}
