689b53d8a12fb4e6c0254ab974ad9ed5bd970e77
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-22 09:40:09 +0200

    Create Get All post shares Use Case

---
+++ SocialMedia/Application/UseCases/Shares/GetAllPostSharesUseCase.cs
@@ -0,0 +1,28 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Shares
+{
+    public class GetAllPostSharesUseCase
+    {
+        private readonly IPost _post;
+
+        public GetAllPostSharesUseCase(IPost post)
+        {
+            _post = post;
+        }
+        public async Task<ResultT<IEnumerable<GetAllPostSharesDTO>>> GetAllPostSharesAsync(long postId)
+        {
+            var result = await _post.getAllPostSharesAsync(postId);
+            if (result.Data.Any())
+                return ResultT<IEnumerable<GetAllPostSharesDTO>>.success(result.Data);
+            return ResultT<IEnumerable<GetAllPostSharesDTO>>.Failure(new List<string> { "No Shares Found" });
+        }
+    }
+}
