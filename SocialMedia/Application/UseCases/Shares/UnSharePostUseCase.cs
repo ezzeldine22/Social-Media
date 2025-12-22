17e9b6a64b67529fb679e3a335b2973bfb2b91d0
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-22 09:40:55 +0200

    Create UnShare post Use Case

---
+++ SocialMedia/Application/UseCases/Shares/UnSharePostUseCase.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Shares
+{
+    public class UnSharePostUseCase
+    {
+        private readonly IPost _post;
+
+        public UnSharePostUseCase(IPost post)
+        {
+            _post = post;
+        }
+        public async Task<Result> UnSharePostAsync(long postId)
+        {
+            var result = await _post.UnsharePostAsync(postId);
+            return result;
+        }
+    }
+}
