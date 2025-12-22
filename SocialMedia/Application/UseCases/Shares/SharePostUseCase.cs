491afa92ce05f5719fe689c29156c239610c8d76
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-22 09:40:46 +0200

    Create Share Post Use Case

---
+++ SocialMedia/Application/UseCases/Shares/SharePostUseCase.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Shares
+{
+    public class SharePostUseCase
+    {
+        private readonly IPost _post;
+
+        public SharePostUseCase(IPost post)
+        {
+            _post = post;
+        }
+        public async Task<Result> SharePostAsync(long postId)
+        {
+            var result = await _post.sharePostAsync(postId);
+            return result;
+        }
+    }
+}
