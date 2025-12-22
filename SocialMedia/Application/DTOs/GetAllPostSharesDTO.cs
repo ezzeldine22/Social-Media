3c844ae23e9ba50d107564d4ef8462fc9712ec04
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-22 09:39:25 +0200

    create get all post shares dto

---
+++ SocialMedia/Application/DTOs/GetAllPostSharesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class GetAllPostSharesDTO
+    {
+        public string userName { get; set; }
+        public string userId { get; set; }
+        public string userPicture { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+    }
+}
